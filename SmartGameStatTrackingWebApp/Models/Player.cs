64db702f807bcd8dea96b7fffec92f3121aebd8d
Colin Legge <colinlegge7@gmail.com>
2018-11-06 10:12:36 -0600

    Table Fixes and Display Bugs

--- SmartGameStatTrackingWebApp/Models/Player.cs
+++ SmartGameStatTrackingWebApp/Models/Player.cs
@@ -11,31 +11,43 @@ namespace SmartGameStatTrackingWebApp.Models
     {
         public int id { get; set; }
 
+        [Display(Name = "#")]
         public int number { get; set; }
 
+        [Display(Name = "Player")]
         public string name { get; set; }
 
+        [Display(Name = "Team")]
         public string team { get; set; }
 
+        [Display(Name = "GP")]
         public int gamesPlayed { get; set; }
 
         [Display(Name = "PPG")]
         public int points { get; set; }
 
+        [Display(Name = "RPG")]
         public int rebounds { get; set; }
 
+        [Display(Name = "APG")]
         public int assists { get; set; }
 
+        [Display(Name = "BPG")]
         public int blocks { get; set; }
 
+        [Display(Name = "SPG")]
         public int steals { get; set; }
 
+        [Display(Name = "TO/G")]
         public int turnovers { get; set; }
 
+        [Display(Name = "PF/G")]
         public int personalFouls { get; set; }
 
+        [Display(Name = "TF/G")]
         public int technicalFouls { get; set; }
 
+        [Display(Name = "Season")]
         [Range(2018, Int32.MaxValue)]
         public int season { get; set; }
     }
